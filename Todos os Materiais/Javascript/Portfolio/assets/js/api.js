bf71e2a0f37996867a3c9a7caa14751841bf53f6
heeydev <maciellbranco@hotmail.comgit config --global user.email maciellbranco@hotmail.comgit config --global user.email maciellbranco@hotmail.com>
2022-12-04 00:28:53 -0300

    Update correção

--- Todos os Materiais/Javascript/Portfolio/assets/js/api.js
+++
@@ -1,7 +0,0 @@
-
-
-async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/heeydev/Meus-Projetos/master/Todos%20os%20Materiais/Javascript/Portfolio/data/profile.json';
-    const fetching = await fetch(url)
-    return await fetching.json()
-}
