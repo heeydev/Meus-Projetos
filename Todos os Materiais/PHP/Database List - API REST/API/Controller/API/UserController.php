9afa2927875140125c0165a416770473e5acab3e
heeydev <maciellbranco@hotmail.comgit config --global user.email maciellbranco@hotmail.comgit config --global user.email maciellbranco@hotmail.com>
2022-12-11 11:41:18 -0300

    Busca Inicial no ViaCep

---
+++ Todos os Materiais/PHP/Database List - API REST/API/Controller/API/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+class UserController extends BaseController{
+
+    public function listAction()
+    {
+        $erroDescription = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $stringParamsArray = $this->getStringParams();
+
+        if (strtoupper($requestMethod) == 'GET') {
+            try {
+                $userModel = new UserModel();
+
+                $intLimit = 10;
+                if(isset($stringParamsArray['limit']) && $stringParamsArray['limit']){
+                    $intLimit = $stringParamsArray['limit'];
+                }
+
+                $usersArray = $userModel->getUsers($intLimit);
+                $responseData = json_encode($usersArray);
+            } catch (Error $e) {
+                $erroDescription = $e->getMessage() .'Something went wrong! Please contact support. ';
+                $errorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        }else {
+            $erroDescription = 'Method not supported';
+            $errorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+
+        //send output
+        if(!$erroDescription){
+            $this->sendOutPut(
+                $responseData, 
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        }else {
+            $this->sendOutPut(json_encode(array('error' => $erroDescription)), 
+            array('Content-Type: application/json', $errorHeader)
+        );
+        }
+    }
+}
